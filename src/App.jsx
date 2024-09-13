8a678e3d67d0f59e392865f9462a1daa5155954c
kompromiss <artemprodos@gmail.com>
2024-09-13 12:55:28 +0200

    ds

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import putin from '/flnwxqh1.png'
+import './novova.css'
+import Header from './katalog/header'
+import Button from './komponent/button'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        <section>
+        </section>
+        <section>
+          <h3>xhxhxjh</h3>
+
+          <Button />
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
